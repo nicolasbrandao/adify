acd747cef1da18b84768a687a12b56940d88b28e
Nícolas Brandão <nicolas.brandaor@gmail.com>
2024-01-29 22:05:01 -0300

    FEA: install sanity

---
+++ app/(studio)/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Adify - Studio",
+  description: "Adify - Studio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
